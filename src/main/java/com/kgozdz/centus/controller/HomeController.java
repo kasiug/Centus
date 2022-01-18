949c93d8fcd98325fd8df0d5fe646979934da702
Katarzyna Gozdz <irmakasia@gmail.com>
2022-01-18 20:06:03 +0100

    Monthly Statement, Yearly Statement, Change monthly limit

--- src/main/java/com/kgozdz/centus/controller/HomeController.java
+++ src/main/java/com/kgozdz/centus/controller/HomeController.java
@@ -69,4 +69,28 @@ public class HomeController {
         stage.setScene(scene);
         stage.show();
     }
+
+    public void onChangeLimitButtonClick(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/com/kgozdz/centus/limit-view.fxml"));
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void onMonthlyStatementButtonClick(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/com/kgozdz/centus/monthly-view.fxml"));
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void onYearlyStatementButtonClick(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/com/kgozdz/centus/monthly-view.fxml"));
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
 }
---
+++ src/main/java/com/kgozdz/centus/controller/YearlyController.java
@@ -0,0 +1,53 @@
+package com.kgozdz.centus.controller;
+
+import com.kgozdz.centus.repository.IUserRepository;
+import com.kgozdz.centus.repository.implementation.UserRepository;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class YearlyController {
+    private IUserRepository userRepository;
+
+    @FXML
+    private Label dateTime;
+
+    @FXML
+    private Button logOutButton;
+
+    @FXML
+    private Hyperlink exitToHomePageLink;
+
+    public YearlyController() {
+        this.userRepository = new UserRepository();
+    }
+    @FXML
+    protected void onExitToHomePageLinkClick(ActionEvent event) throws IOException {
+        this.moveToHomePage(event);
+    }
+
+    public void moveToHomePage(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/com/kgozdz/centus/home-view.fxml"));
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void onLogOutButtonClick(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/com/kgozdz/centus/login-view.fxml"));
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+}
